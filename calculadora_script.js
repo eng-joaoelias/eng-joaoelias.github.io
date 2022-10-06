d384a3b5798def2b52e46da4102abee9b6febecc
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2022-10-06 16:40:25 -0300

    Update calculadora_script.js
    Atualização para tratar as indeterminações matemáticas.

--- calculadora_script.js
+++ calculadora_script.js
@@ -11,19 +11,24 @@ function inserirDadosDisplay(component) {
     }
     if (component.value === "←") {
         let exibicao = tela.value
-        exibicao = exibicao.substr(0, exibicao.length - 1);
-        tela.value = exibicao;
+        exibicao = exibicao.substr(0, exibicao.length - 1); //apaga o que esta escrito na tela
+        tela.value = exibicao; //exibe na tela o texto da expressao ja apagado
     }
 }
 
 function resultadoExpressao(component) {
     let tela = document.querySelector(".display");
-    if (tela) {
-        if (tela.value === "" && component.value === "=") {
+    if (tela) { //verifica se a tela esta disponivel
+        if (tela.value === "" && component.value === "=") { //impede que o usuario aperte o botao igual sem ter expressao na tela
             tela.value = "";
         }
         else {
-            tela.value = eval(tela.value);
+            if (eval(tela.value) == Number.POSITIVE_INFINITY || eval(tela.value) == Number.NEGATIVE_INFINITY || eval(tela.value) == NaN) {
+                tela.value = "Ind."; //tratamento de indeterminacoes matematicas
+            }
+            else {
+                tela.value = eval(tela.value); //expressao matematica valida
+            }
         }
     }
 }
