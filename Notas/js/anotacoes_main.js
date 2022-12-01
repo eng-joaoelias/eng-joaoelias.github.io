a2280f8652a32e680f6eb5b6367203010db0d766
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2022-12-01 16:34:52 -0300

    Removido o app de Anotações
    Retirada para manutenção.

--- Notas/js/anotacoes_main.js
+++
@@ -1,9 +0,0 @@
-import AnotacoesAPI from "./AnotacoesAPI.js";
-import AnotacoesView from "./AnotacoesView.js";
-
-const aplicacao = document.querySelector("#app");
-const view = new AnotacoesView(aplicacao, {
-    quandoNotaSelect() {
-
-    }
-});
