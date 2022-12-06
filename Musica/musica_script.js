40b0b223318c9d5b67703f631de39b842c24d25b
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2022-12-06 14:24:59 -0300

    Projeto em fase final de dev
    Agora, há 5 musicas (que não são minhas) na base de dados. É possível avançar entre as músicas, pausar e iniciar a reprodução. Ao avançar, a música começa a ser reproduzida automaticamente.

--- Musica/musica_script.js
+++ Musica/musica_script.js
@@ -1,4 +1,45 @@
+let musicas = [
+    {titulo: "Play With Me", artista: "Extreme", album: "Extreme", src: "assets/musics/Extreme Play With Me.mp3", img: "assets/images/extreme.jfif"},
+    {titulo: "Cum on Feel the Noize", artista: "Quiet Riot", album: "Metal Health", src: "assets/musics/Quiet Riot Cum on Feel the Noize.mp3", img: "assets/images/metal_health.jpg"},
+    {titulo: "Round and Round", artista: "Ratt", album: "Out Of The Cellar", src: "assets/musics/Ratt Round and Round.mp3", img: "assets/images/out_of_cellar.jpg"},
+    {titulo: "In My Dreams", artista: "Dokken", album: "Under Lock and Key", src: "assets/musics/Dokken - In My Dreams.mp3", img: "assets/images/under_lock_and_key.jpg"},
+    {titulo: "Keep on Loving You", artista: "REO Speedwagon", album: "Hi Infidelity", src: "assets/musics/REO Speedwagon - Keep on Loving You.mp3", img: "assets/images/Hi_Infidelity.jpg"}
+];
+
 let musica = document.querySelector("audio");
+
+let posicaoMusica = 0;
+
+let capaAlbum = document.querySelector("img");
+
+let nomeMusica = document.querySelector(".descricao h2");
+
+let nomeArtista = document.querySelector(".descricao i");
+
+renderizarMusica(posicaoMusica);
+
+document.querySelector("#anterior").addEventListener("click", () => {
+    if (posicaoMusica>0) {
+        posicaoMusica--;
+        renderizarMusica(posicaoMusica);
+    }
+    else{
+        window.alert("INÍCIO DA LISTA!\n")
+        renderizarMusica(posicaoMusica);
+    }
+    mudaEstiloPlay();
+});
+
+document.querySelector("#proxima").addEventListener("click", () => {
+    if (posicaoMusica < (musicas.length) - 1) {
+        posicaoMusica++;
+        renderizarMusica(posicaoMusica);
+        mudaEstiloPlay();
+    }
+    else{
+        window.alert("FIM DA LISTA!\n");
+    }
+});
 
 musica.addEventListener("timeupdate", ()=>{
     avancaDuracaoAudio();
@@ -12,8 +53,22 @@ document.querySelector("#botao-pause").addEventListener("click", ()=>{
     pausarMusica();
 });
 
+function renderizarMusica(index) {
+    musica.setAttribute("src", musicas[index].src);
+    musica.addEventListener("loadeddata", () => {
+        nomeMusica.textContent = musicas[index].titulo;
+        nomeArtista.textContent = musicas[index].artista;
+        capaAlbum.src = musicas[index].img;
+        document.querySelector(".fim").textContent = segundosParaMinutos(Math.floor(musica.duration));
+    });
+}
+
 function tocarMusica() {
     musica.play();
+    mudaEstiloPlay();
+}
+
+function mudaEstiloPlay() {
     document.querySelector("#botao-pause").style.display = "block";
     document.querySelector(".botao-pause").style.display = "block";
     document.querySelector("#botao-play").style.display = "none";
@@ -21,6 +76,10 @@ function tocarMusica() {
 
 function pausarMusica(){
     musica.pause();
+    mudaEstiloPause();
+}
+
+function mudaEstiloPause() {
     document.querySelector("#botao-play").style.display = "block";
     document.querySelector(".botao-play").style.display = "block";
     document.querySelector("#botao-pause").style.display = "none";
@@ -32,6 +91,8 @@ function avancaDuracaoAudio(){
         document.querySelector("progress").style.width = percentualDucaraoMusica + "%";
         let tempoDecorrido = document.querySelector(".inicio");
         tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime));
+        let duracaoMusica = document.querySelector(".fim");
+        duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration));
     }, 0.001);
 }
 
