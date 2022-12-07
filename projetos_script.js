f822091500f24164b26564f11428d9b5344f02cb
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2022-12-07 16:37:23 -0300

    Criado script para a página de projetos

---
+++ projetos_script.js
@@ -0,0 +1,140 @@
+const btnSlide01 = document.querySelector("#btnslide1");
+const btnSlide02 = document.querySelector("#btnslide2");
+const btnSlide03 = document.querySelector("#btnslide3");
+const btnSlide04 = document.querySelector("#btnslide4");
+const btnSlide05 = document.querySelector("#btnslide5");
+const btnSlide06 = document.querySelector("#btnslide6");
+const btnSlide07 = document.querySelector("#btnslide7");
+const btnSlide08 = document.querySelector("#btnslide8");
+const btnSlide09 = document.querySelector("#btnslide9");
+const btnSlide10 = document.querySelector("#btnslide10");
+
+btnSlide01.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "#295d09";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide02.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "#295d09";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide03.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "#295d09";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide04.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "#295d09";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide05.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "#295d09";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide06.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "#295d09";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide07.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "#295d09";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide08.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "#295d09";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide09.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "#295d09";
+    btnSlide10.style.backgroundColor = "rgba(0,0,0,0)";
+});
+
+btnSlide10.addEventListener("click", () => {
+    btnSlide01.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide02.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide03.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide04.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide05.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide06.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide07.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide08.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide09.style.backgroundColor = "rgba(0,0,0,0)";
+    btnSlide10.style.backgroundColor = "#295d09";
+});
