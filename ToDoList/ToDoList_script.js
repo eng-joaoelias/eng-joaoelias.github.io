0ae0ee1986982000bd7933a30c45b4619d4e646d
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-02-07 16:37:13 -0300

    Update ToDoList_script.js

--- ToDoList/ToDoList_script.js
+++ ToDoList/ToDoList_script.js
@@ -1,10 +1,10 @@
-function criarItem(){
+function criarItem(tarefa, status = ''){
     const item = document.createElement('label');
     item.classList.add("todo__item");
 
     item.innerHTML = `
-        <input type = "checkbox">
-        <div></div>
+        <input type = "checkbox" ${status}>
+        <div>${tarefa}</div>
         <input type = "button" value = "X">
     `
 
