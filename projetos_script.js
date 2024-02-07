4f711f4383fe33cceb0e9f9fb330a28a1720b447
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-02-07 15:36:55 -0300

    Update projetos_script.js
    Neste código, eu adicionei duas novas funções: iniciarTimer() e reiniciarTimer(). A função iniciarTimer() é chamada no início e define o temporizador para chamar a função proxProjeto() a cada 7500 milissegundos. A função reiniciarTimer() é chamada quando o usuário clica nos botões "anterior" ou "próximo". Ele limpa o temporizador existente e o configura novamente, reiniciando assim o temporizador. Isso garante que o temporizador seja reiniciado sempre que o usuário interagir com os botões de navegação.

--- projetos_script.js
+++ projetos_script.js
@@ -1,23 +1,36 @@
-let slides = document.querySelectorAll('.slide-container');
-
+let slides = document.querySelectorAll(".slide-container");
 let index = 0;
+let timer;
 
 function proxProjeto() {
-    slides[index].classList.remove('activ');
+    slides[index].classList.remove("activ");
     index = (index + 1) % slides.length;
-    slides[index].classList.add('activ');
+    slides[index].classList.add("activ");
+    reiniciarTimer();
 }
 
 function prevProjeto() {
     slides[index].classList.remove("activ");
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add("activ");
+    reiniciarTimer();
 }
 
-setInterval(() => {
+function iniciarTimer() {
+    timer = setInterval(proxProjeto, 7500);
+}
+
+function reiniciarTimer() {
+    clearInterval(timer);
+    iniciarTimer();
+}
+
+iniciarTimer();
+
+document.querySelector("#anterior").addEventListener("click", () => {
+    prevProjeto();
+});
+
+document.querySelector("#proximo").addEventListener("click", () => {
     proxProjeto();
-}, 45000);
-
-document.querySelector("#anterior").addEventListener("click", prevProjeto);
-
-document.querySelector("#proximo").addEventListener("click", proxProjeto);
+});
