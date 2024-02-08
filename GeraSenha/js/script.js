7333e9f0f00f03a1ba78db441f5ec35f676bddf6
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-02-08 14:24:43 -0300

    Update script.js
    Geração de letra minúscula e letra maiúscula.

--- GeraSenha/js/script.js
+++ GeraSenha/js/script.js
@@ -4,5 +4,16 @@ const btnGeraSenha = document.querySelector("#generatepassword");
 const btnSenhaGerada = document.querySelector("#generated-password");
 
 //Funcoes
+const getLetraMinuscula = () =>{
+    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+}
+
+const getLetraMaiuscula = () =>{
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
 
 //Eventos
+btnGeraSenha.addEventListener("click", () => {
+
+}
+);
