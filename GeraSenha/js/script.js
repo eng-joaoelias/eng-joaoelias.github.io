865a77cbc5725948183e8ce37cbd9bc8da64c64c
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-02-08 14:15:21 -0300

    Update script.js

--- GeraSenha/js/script.js
+++ GeraSenha/js/script.js
@@ -1,1 +1,8 @@
+//Selecao de Elementos
 
+const btnGeraSenha = document.querySelector("#generatepassword");
+const btnSenhaGerada = document.querySelector("#generated-password");
+
+//Funcoes
+
+//Eventos
